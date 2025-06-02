115e19d4b91e8004c402352c82c8d85d9f8adff2
megz15 <meghraj.g16@gmail.com>
2025-06-02 22:07:49 +0530

    refactor: alternate command parser

--- src/routes/terminal/+page.svelte
+++ src/routes/terminal/+page.svelte
@@ -6,47 +6,37 @@
 
     onMount(() => {
         window.onload = () => {
-            window.addEventListener('keydown', (e) => {
+            window.addEventListener("keydown", (e) => {
                 let cmd = cmdInput.textContent!;
                 if (e.key.length == 1) {
                     cmdInput.textContent += e.key;
-                } else if (e.key == 'Backspace') {
+                } else if (e.key == "Backspace") {
                     cmdInput.textContent = cmd.slice(0, -1);
-                } else if (e.key == 'Enter') {
-                    let output = '';
+                } else if (e.key == "Enter") {
                     cmd = cmd.trim();
+                    let output = parseCommand(cmd);
 
-                    if (cmd in commands) {
-                        output = commands[cmd];
-                    } else if (cmd == 'clear') {
-                        outputHistory.innerHTML = '';
-                        cmdInput.textContent = '';
-                        return;
-                    } else if (cmd == '') {
-                        outputHistory.innerHTML += `<pre>${promptText} ${cmd}</pre>`;
-                        return;
-                    } else {
-                        output = `MESh: command not found: ${cmd}`;
-                    }
-
-                    outputHistory.innerHTML += `<pre>${promptText} ${cmd}</div><pre class="text-base/4"> &gt&gt ${output}</pre>`;
-                    cmdInput.textContent = '';
+                    if (cmd != "clear" && cmd != "") outputHistory.innerHTML += `<pre>${promptText} ${cmd}</div><pre class="text-base/4"> &gt&gt ${output}</pre>`;
+                    cmdInput.textContent = "";
 
                 }
             })
         }
     });
 
-    let promptText = 'oh no march is in ~ $';
+    let userName = "march";
 
-    const commands: {[key: string]:string} = {
-        'help': `Available commands:
-    banner - Show banner
-    clear - Clear the terminal output
-    help - Show this message
-    ls - List files in the current directory
-    whoami - Show the current user`,
-        'banner': `
+    let promptText = `oh no ${userName} is in ~ $`;
+
+    function parseCommand(cmd: string): string {
+        switch (cmd) {
+
+            case "":
+                outputHistory.innerHTML += `<pre>${promptText} ${cmd}</pre>`;
+                return "";
+            
+            case "banner":
+                return `
 
     ##::::'##:'########::'######::'##::::'##:
     ###::'###: ##.....::'##... ##: ##:::: ##:
@@ -60,16 +50,35 @@
     Megz's Emulated Terminal Shell - v0.0.1
     Type 'banner' to see this message again
     Type 'help' for a list of commands
-        `,
-        'ls': 'cat.img\tcat.jpg\tcat.txt',
-        'whoami': 'march',
+                `;
+            
+            case "clear":
+                outputHistory.innerHTML = "";
+                cmdInput.textContent = "";
+                return "";
+            
+            case "help":
+                return `Available commands:
+    banner - Show banner
+    clear - Clear the terminal output
+    help - Show this message
+    ls - List files in the current directory
+    pwd - Print current working directory
+    whoami - Show the current user`;
+            
+            case "whoami":
+                return userName;
+
+            default:
+                return `MESh: command not found: ${cmd}`;
+        }
     }
 </script>
 
 <div class="input">
     <div class="output-history" bind:this={outputHistory}>
-        <pre class="text-base/4">{commands["banner"]}</pre>
+        <pre class="text-base/4">{parseCommand("banner")}</pre>
     </div>
-    <br><span class="prompt">{promptText}</span>
+    <span class="prompt">{promptText}</span>
     <span class="cmd-input break-all" bind:this={cmdInput} ></span>
 </div>
