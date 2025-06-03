df6cf2a77ee20d14cbf446dc23cf9ad96f316944
megz15 <meghraj.g16@gmail.com>
2025-06-03 18:22:46 +0530

    feat: added a few more commands :p

--- src/routes/terminal/+page.svelte
+++ src/routes/terminal/+page.svelte
@@ -37,7 +37,7 @@
                     case "Enter":
                         outputHistory.innerHTML += `<pre>${promptText} ${cmd.join(" ")}`
                         let output = parseCommand(cmd[0], cmd.slice(1));
-                        if (cmd[0] != "clear" && cmd[0] != "" && cmd[0] != "cd") outputHistory.innerHTML += `</div><pre class="text-base/4"> &gt&gt ${output}</pre>`;
+                        if (cmd[0] != "clear" && cmd[0] != "" && cmd[0] != "cd") outputHistory.innerHTML += `</div><pre class="text-base/5 break-all whitespace-pre-wrap"> &gt&gt ${output}</pre>`;
                         cmdInputText = "";
                         cursorPosition = 0;
                         break;
@@ -138,10 +138,34 @@
     Type 'help' for a list of commands
                 `;
             
+            case "help":
+                return `Available commands:
+    banner \t\t\t\t Show banner
+    [ WIP ] cat \t\t View file contents (not implemented yet)
+    cd [directory] \t\t Change the current working directory
+    clear \t\t\t\t Clear the terminal output
+    cowsay \t\t\t\t cow ASCII! 🐮
+    [ WIP ] echo \t\t Print text to the terminal (not implemented yet)
+    emacs \t\t\t\t The extensible, customizable, self-documenting real-time display editor
+    exit \t\t\t\t Exit the terminal (buggy)
+    help \t\t\t\t Show this message
+    ls [directory] \t\t List files in the specified directory (or current directory if none specified)
+    [ WIP ] matrix \t\t 🚫🥄 (not implemented yet)
+    nano \t\t\t\t Pico editor clone with enhancements
+    neofetch \t\t\t CLI system information tool
+    [ WIP ] pacman \t\t Package manager (not implemented yet)
+    pwd \t\t\t\t Print current working directory
+    [ WIP ] resume \t\t Show resume (not implemented yet)
+    sudo [command] \t\t Run a command with superuser privileges
+    test \t\t\t\t O_O
+    [ WIP ] theme \t\t Change the terminal theme (not implemented yet)
+    vim \t\t\t\t Vi Improved, a highly configurable, improved version of the vi text editor
+    whoami \t\t\t\t Show the current user`;
+            
             case "cd":
                 if (args.length != 0 && args[0] != ".") {
                     if (args.length > 1) {
-                        outputHistory.innerHTML += `</div><pre class="text-base/4"> &gt&gt cd: too many arguments</pre>`;
+                        outputHistory.innerHTML += `</div><pre class="text-base/5 break-all whitespace-pre-wrap"> &gt&gt cd: too many arguments</pre>`;
                     } else if (args[0] == "..") {
                         if (workingDirectoryPath == "/") {
                         } else {
@@ -165,25 +189,11 @@
                         });
 
                         if (!pathExists) {
-                            outputHistory.innerHTML += `</div><pre class="text-base/4"> &gt&gt cd: not a directory: ${requiredDirPath}</pre>`;
+                            outputHistory.innerHTML += `</div><pre class="text-base/5 break-all whitespace-pre-wrap"> &gt&gt cd: not a directory: ${requiredDirPath}</pre>`;
                         } else workingDirectoryPath = requiredDirPath;
                     }
                 }
                 return "";
-            
-            case "clear":
-                outputHistory.innerHTML = "";
-                cmdInputText = "";
-                return "";
-            
-            case "help":
-                return `Available commands:
-    banner - Show banner
-    clear - Clear the terminal output
-    help - Show this message
-    ls - List files in the current directory
-    pwd - Print current working directory
-    whoami - Show the current user`;
 
             case "ls":
                 let requiredDir: VirtualDirectory = virtualFilesystem[""];
@@ -215,11 +225,46 @@
                     return `ls: cannot access '${requiredDirPath}': No such file or directory`;
                 } else return ["", ".", "..", ...Object.keys(requiredDir).filter(dir => dir != "files"), ...requiredDir.files || []].join("\n");
             
+            case "clear":
+                outputHistory.innerHTML = "";
+                cmdInputText = "";
+                return "";    
+
             case "pwd":
                 return workingDirectoryPath;
             
             case "whoami":
                 return userName;
+            
+            case "cowsay":
+                return `
+^__^
+(oo)\\_______
+(__)\\       )\\/\\
+    ||----w |
+    ||     ||`;
+
+            case "sudo":
+                if (args.length == 0) {
+                    return "sudo: no command specified";
+                } else {
+                    return "I'm sorry, Dave. I'm afraid I can't do that.";
+                }
+            
+            case "exit":
+                return "One does not simply exit MESh.";
+            
+            case "neofetch":
+                return "Neofetch has been deprecated.\nPlease use 'banner' instead.";
+            
+            case "vim":
+                return "User will not be able to exit vim.\n >> Please use 'emacs' instead.";
+            
+            case "emacs":
+                return "Too many packages NOT ENOUGH RAM\n >> Please use 'nano' instead.";
+            
+            case "nano":
+                return "User is too advanced for nano.\n >> Please use 'vim' instead.";
 
             default:
                 return `MESh: command not found: ${cmd}`;
@@ -244,7 +289,7 @@
 
 <div class="input">
     <div class="output-history" bind:this={outputHistory}>
-        <pre class="text-base/4">{parseCommand("banner")}</pre>
+        <pre class="text-base/5 break-all whitespace-pre-wrap">{parseCommand("banner")}</pre>
     </div>
     <span class="prompt">{promptText}</span>
     <span class="cmd-input break-all">{@html highlightCursor(cmdInputText, cursorPosition)}</span>
