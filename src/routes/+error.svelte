e38037b4caa246f048e709baa113ccfceeb25c18
Meghraj Goswami <meghraj.g16@gmail.com>
2025-12-29 18:40:43 +0530

    style: error page achievement

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import error from "$lib/assets/error.webp"
+    import { page } from '$app/state'
+</script>
+
+<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-8">
+    <img src={error} alt='error achievement' class="w-96" />
+    <div class="text-3xl">{page.status}: {page.error?.message}</div>
+</div>
